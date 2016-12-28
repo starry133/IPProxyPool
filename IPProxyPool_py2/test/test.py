15f61d9ff3e82cc2587b8734c0352c1938c89efc
starry133 <liyabupt@163.com>
2016-12-28 08:49:20 +0800

    rewrite the test.py to verify the ip is available or not

--- IPProxyPool_py2/test/test.py
+++ IPProxyPool_py2/test/test.py
@@ -1,15 +1,23 @@
+#!/usr/bin/env python
 #coding:utf-8
 import requests
 import json
-r = requests.get('http://127.0.0.1:8000/?types=0&count=5&country=中国')
+r = requests.get('http://127.0.0.1:8000/?')
 ip_ports = json.loads(r.text)
 print ip_ports
-ip = ip_ports[0]['ip']
-port = ip_ports[0]['port']
-proxies={
-    'http':'http://%s:%s'%(ip,port),
-    'https':'http://%s:%s'%(ip,port)
-}
-r = requests.get('http://ip.chinaz.com/',proxies=proxies)
-r.encoding='utf-8'
-print r.text
+i = 0
+https_ips = []
+for ip_port in ip_ports:
+    print ip_port
+    ip = ip_port[0]
+    port = ip_port[1]
+    proxies={
+        'https':'http://%s:%s'%(ip,port)
+    }
+    try:
+        r = requests.get('https://list.jd.com/list.html?cat=9987,653,655',proxies=proxies, timeout=8)
+        https_ips.append(ip_port)
+    except requests.exceptions.RequestException:
+        print 'cant access to JD!'
+print len(https_ips)
+print https_ips
