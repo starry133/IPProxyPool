509134e2b855501c113d7eba6229cdab9f0a201b
starry133 <liyabupt@163.com>
2016-12-31 12:11:10 +0800

    fix small bugs

--- IPProxyPool_py2/test/test.py
+++ IPProxyPool_py2/test/test.py
@@ -23,7 +23,7 @@ for ip_port in ip_ports:
     '''
     proxies={
         'http':'http://%s:%s'%(ip,port),
-        'https':'http://%s:%s'%(ip,port)
+        'https':'https://%s:%s'%(ip,port)
     }
     try:
         requests.get('https://list.jd.com/list.html?cat=9987,653,655',proxies=proxies, timeout=6)
@@ -32,10 +32,11 @@ for ip_port in ip_ports:
         r = requests.get(url='http://ipaddress.com/')
         root = etree.HTML(r.text)
         proxy = root.xpath('/html/body/div[1]/div[3]/div[2]/table/tr[6]/td/text()')[0]
-        if proxy = 'No Proxy Detected':
-        https_anony_ips.append(ip_port)
+        if proxy == 'No Proxy Detected':
+            https_anony_ips.append(ip_port)
     except requests.exceptions.RequestException:
         print 'cant access to JD!'
+
 print len(https_ips)
 print https_ips
 print https_anony_ips
